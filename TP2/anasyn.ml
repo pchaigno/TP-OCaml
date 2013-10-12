102d4031180336fdf7af4b18c36233d5798d3beb
Paul Chaignon <paul.chaignon@insa-rennes.fr>
2013-10-12 17:08:53 +0200

    Bug fixed: error in grammar.

--- TP2/anasyn.ml
+++ TP2/anasyn.ml
@@ -43,41 +43,43 @@ exception MatchException of arbre_concret;;
 let derivation = function
   | (FILE, _) -> [Vn EXPR; Vt UL_EOF]
   | (EXPR, (UL_PAROUV | UL_SI | UL_IDENT _)) -> [Vn TERMB; Vn SUITEEXPR]
-  | (SUITEEXPR, UL_OU) -> [Vt UL_OU; Vn TERMB; Vn SUITEEXPR]
+  | (SUITEEXPR, UL_OU) -> [Vt UL_OU; Vn EXPR]
   | (SUITEEXPR, (UL_EOF | UL_ALORS | UL_SINON | UL_FSI)) -> []
   | (TERMB, (UL_PAROUV | UL_SI | UL_IDENT _)) -> [Vn FACTEURB; Vn SUITETERMB]
-  | (SUITETERMB, UL_ET) -> [Vt UL_ET; Vn FACTEURB; Vn SUITETERMB]
+  | (SUITETERMB, UL_ET) -> [Vt UL_ET; Vn TERMB]
   | (SUITETERMB, (UL_OU | UL_EOF | UL_ALORS | UL_SINON | UL_FSI)) -> []
   | (FACTEURB, UL_PAROUV) -> [Vt UL_PAROUV; Vn EXPR; Vt UL_PARFERM]
   | (FACTEURB, UL_SI) -> [Vt UL_SI; Vn EXPR; Vt UL_ALORS; Vn EXPR; Vt UL_SINON; Vn EXPR; Vt UL_FSI]
   | (FACTEURB, UL_IDENT _) -> [Vn RELATION]
   | (RELATION, UL_IDENT x) -> [Vt (UL_IDENT x); Vn OP; Vt (UL_IDENT x)]
   | (OP, ((UL_EGAL | UL_DIFF | UL_SUP | UL_INF | UL_SUPEGAL | UL_INFEGAL) as x)) -> [Vt x]
-  | (OP, x) -> raise(DerivationException(OP, x));;
+  | (x, y) -> raise(DerivationException(x, y));;
 
 let rec analyse_caractere = function
   | ((Vt _), p::r) -> (Leaf p, r)
   | (Vn nterm, p::r) -> let listeTerm = derivation (nterm, p) in
 			let (listeV, listeUL) = analyse_mot(listeTerm, p::r) in
 			((Node (nterm, listeV)), listeUL)
+  | (_, []) -> raise (Failure "analyse_caractere")
 and analyse_mot = function
   | ([], liste) -> [], liste
   | (pTerm::rTerm, listeUL) -> let (ac, newList) = analyse_caractere(pTerm, listeUL) in
                                let (suiteListeV, newList) = analyse_mot(rTerm, newList) in
                                (ac::suiteListeV, newList);;
 
-let (arbConc, listeVide) = analyse_caractere(Vn FILE, [UL_IDENT "t"; UL_SUP; UL_IDENT "y"; UL_ET; UL_IDENT "x"; UL_EGAL; UL_IDENT "y"; UL_EOF]);;
+let (arbConc, listeVide) = analyse_caractere (Vn FILE, [UL_IDENT "t"; UL_SUP; UL_IDENT "y"; UL_ET; UL_IDENT "x"; UL_EGAL; UL_IDENT "y"; UL_EOF]);;
+let (arbConc1, listeVide1) = analyse_caractere (Vn FILE, [UL_IDENT "t"; UL_SUP; UL_IDENT "y"; UL_ET; UL_IDENT "t"; UL_SUP; UL_IDENT "y"; UL_EOF]);;
 
-let rec construire_arbre_abstrait = function
-  | Node (FILE, [a; Leaf UL_EOF]) -> construire_arbre_abstrait a
-  | Node (FACTEURB, [Leaf UL_PAROUV; a; Leaf UL_PARFERM]) -> construire_arbre_abstrait a
-  | Node (FACTEURB, [a]) -> construire_arbre_abstrait a
-  | Node (TERMB, [a; Node (SUITETERMB, [])]) -> construire_arbre_abstrait a
-  | Node (EXPR, [a; Node (SUITEEXPR, [])]) -> construire_arbre_abstrait a
-  | Node (EXPR, [a; Node (SUITEEXPR, [Leaf UL_OU; b])]) -> Ou (construire_arbre_abstrait a, construire_arbre_abstrait b)
-  | Node (TERMB, [a; Node (SUITETERMB, [Leaf UL_ET; b])]) -> Et (construire_arbre_abstrait a, construire_arbre_abstrait b)
+let rec abstraire = function
+  | Node (FILE, [a; Leaf UL_EOF]) -> abstraire a
+  | Node (EXPR, [a; Node (SUITEEXPR, [Leaf UL_OU; b])]) -> Ou (abstraire a, abstraire b)
+  | Node (EXPR, [a; Node (SUITEEXPR, [])]) -> abstraire a
+  | Node (TERMB, [a; Node (SUITETERMB, [Leaf UL_ET; b])]) -> Et (abstraire a, abstraire b)
+  | Node (TERMB, [a; Node (SUITETERMB, [])]) -> abstraire a
   | Node (RELATION, [Leaf (UL_IDENT ident_a); Node(OP, [Leaf op]); Leaf (UL_IDENT ident_b)]) -> Comp(ident_a, op, ident_b)
-  | Node (FACTEURB, [Leaf UL_SI; a; Leaf UL_ALORS; b; Leaf UL_SINON; c; Leaf UL_FSI]) -> Cond(construire_arbre_abstrait a, construire_arbre_abstrait b, construire_arbre_abstrait c)
+  | Node (FACTEURB, [Leaf UL_PAROUV; a; Leaf UL_PARFERM]) -> abstraire a
+  | Node (FACTEURB, [a]) -> abstraire a
+  | Node (FACTEURB, [Leaf UL_SI; a; Leaf UL_ALORS; b; Leaf UL_SINON; c; Leaf UL_FSI]) -> Cond(abstraire a, abstraire b, abstraire c)
   | x -> raise(MatchException x);; 
 
-let arbAbs = construire_arbre_abstrait arbConc;;
+let arbAbs = abstraire arbConc1;;

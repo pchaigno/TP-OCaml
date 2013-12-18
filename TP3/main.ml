17dd7261722e68ab52fa639375d04e86d2fed63e
unknown <raphael.baron@insa-rennes.fr>
2013-12-18 20:23:12 +0100

    Modif du TP3 : gestion des parse error. A compléter

--- TP3/main.ml
+++ TP3/main.ml
@@ -1,11 +1,10 @@
-(*#use "parser.ml";;
-#use "lexer.ml";;*)
+#use "lexer.ml";;
 
 (* si le fichier "test" contient un code à analyser *)
 (*main get_token (Lexing.from_channel (open_in "test"));;*)
 
 (* pour de tous petits tests *)
-(*main get_token (Lexing.from_string "begin int a ; a <- b + c end");;
+main get_token (Lexing.from_string "begin int a ; a <- b + c end");;
 
 (* La suite est utile pour tester l'analyseur lexical tout seul. *)
 (* Il faut juste déclarer le type token. Normalement il est déclaré au début du *)
@@ -16,4 +15,3 @@ let lexbuf = Lexing.from_channel (open_in "test");;
 (* let lexbuf = Lexing.from_string "begin int a ; a <- b + c end";; *)
 
 get_token lexbuf;; (* donne la prochaine unité lexicale *)
-*)
